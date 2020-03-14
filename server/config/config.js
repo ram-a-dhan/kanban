974de418c64a6f8ca9ce9cf5ae9443b77ea663ed
ram-a-dhan <ram.adhan@outlook.com>
2020-03-15 01:58:41 +0700

    deploy retry;

--- server/config/config.js
+++ server/config/config.js
@@ -16,11 +16,11 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+    "username": "cygtlsenofwmrw",
+    "password": "a517bed4f6d417bb068591b0c025618ba068d774081f6eb9c029a7d4562842e0",
+    "database": "de24hjskoeero6",
+    "host": "ec2-52-202-185-87.compute-1.amazonaws.com",
+    "dialect": "postgres",
     "operatorsAliases": false
   }
 }
