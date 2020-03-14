6496c33bb176d18b709ac66228e07e7524ff427a
ram-a-dhan <ram.adhan@outlook.com>
2020-03-15 03:29:35 +0700

    deploy

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const gauthOption = {
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    scope: 'profile email',
-    prompt: 'select_account'
+    clientId: '530807465865-mbfk02u5k4m9o2988r14hte7v74kss0s.apps.googleusercontent.com'
 }
 
 Vue.use(GAuth, gauthOption)
